58f2bb341804ccb223e41a9824fc67717f64990c
Aurnab Das <aurnabdas@Aurnabs-MBP.lan>
2022-03-03 16:53:12 -0500

    last change

--- main.cpp
+++ main.cpp
@@ -3,21 +3,13 @@
 
 int main()
 {
-  bool n = isDivisibleBy(100,25) 
-  std::cout << "isDivisibleBy" << n << "\n";
-  std::cout << nextPrime(14) << "\n";
-  std::cout << countPrimes(2,11) << "\n";
-  std::cout << isTwinPrime(3) << "\n";
-  std::cout << nextTwinPrime(23) << "\n";
-
-  std::cout << largestTwinPrime(5, 18) << "\n";
-  std::cout << largestTwinPrime(1, 31) << "\n";
-  std::cout << largestTwinPrime(14, 16) << "\n";
-
-
-
-  
-  
+  std::cout << "isDivisibleBy(100,25): " << isDivisibleBy(100,25) << "\n";
+  std::cout << "isPrime(5): " << isPrime(5) << "\n";
+  std::cout << "nextPrime(14): "<< nextPrime(14) << "\n";
+  std::cout << "countPrimes(2,11):" << countPrimes(2,11) << "\n";
+  std::cout << "isTwinPrime(3): " << isTwinPrime(3) << "\n";
+  std::cout << "nextTwinPrime(23): "<< nextTwinPrime(23) << "\n";
+  std::cout << "largestTwimPrime(5,18): "<< largestTwinPrime(5, 18) << "\n";
 
   return 0;
 }
